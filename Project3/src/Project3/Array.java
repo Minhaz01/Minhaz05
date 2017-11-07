0604f5fe826f7ce355e3ccc40635c59df80e11f8
Minhaz01 <33205845+Minhaz01@users.noreply.github.com>
2017-11-07 11:10:00 -0600

    Update Array.java
    hi still trying

--- Project3/src/Project3/Array.java
+++ Project3/src/Project3/Array.java
@@ -4,7 +4,9 @@ public class Array {
 
 	public static void main(String[] args) {
 	System.out.println(" This is my own try for Git");
-	System.out.println(" This is my own try for Git2");	
+	System.out.println(" This is my own try for Git2");
+	System.out.println(" This is my own try for Git3");	
+	System.out.println(" This is my own try for Git4");	
 	}
 
 }
